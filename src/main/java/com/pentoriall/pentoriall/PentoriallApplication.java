983871840275b33d4a7c7bf958e27b542c7b3bb8
DongHwan Ko <111329365+eurdream98@users.noreply.github.com>
2024-03-19 17:17:38 +0900

    #1 기본 프로젝트 추가 (#2)

---
+++ src/main/java/com/pentoriall/pentoriall/PentoriallApplication.java
@@ -0,0 +1,13 @@
+package com.pentoriall.pentoriall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PentoriallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PentoriallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pentoriall/pentoriall/PentoriallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pentoriall.pentoriall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PentoriallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
