902877a71ed0526ada7307b630e21442625ba95a
아현 Ahyeon <aha9654@gmail.com>
2024-03-22 17:49:55 +0900

    #20, #21 포인트 충전 및 거래 패키지 구조 (#27)
    * feat: 포인트 충전, 결제 관련 기본 패키지
    
    * feat: DTO 필드 추가

---
+++ src/main/java/com/pentoryall/point/dto/OrderDTO.java
@@ -0,0 +1,19 @@
+package com.pentoryall.point.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class OrderDTO {
+
+    Long code;
+    Long userCode;
+    int amount;
+    int point;
+    LocalDateTime createDate;
+}
