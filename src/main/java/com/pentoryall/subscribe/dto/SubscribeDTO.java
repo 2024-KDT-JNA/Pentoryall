5e88da7a10008aa210b582371de6b0ead52a7f9c
seungjaeshina <seungjaeshin96@gmail.com>
2024-03-22 18:05:47 +0900

    feat: membershipDto 데이터 타입 변경/ MembershipJoinDTO 추가

--- src/main/java/com/pentoryall/subscribe/dto/SubscribeDTO.java
+++ src/main/java/com/pentoryall/subscribe/dto/SubscribeDTO.java
@@ -4,26 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.time.LocalDateTime;
-
 @Getter
 @Setter
 @ToString
 public class SubscribeDTO {
-    private int code;
-    private int userCode;
-    private String name;
-    private String introduction;
-    private int price;
-    private String color;
-    private boolean isActive;
-    private boolean isDeleted;
-    private LocalDateTime createDate;
-    private LocalDateTime updateDate;
-    private LocalDateTime deleteDate;
-
-    public SubscribeDTO() {
-
-    }
+    private long code;
+    private long userCode;
+    private long subscribeUserCode;
 
 }
