434787863a0e97b53a8d77c48fdd855b98d45de5
아현 Ahyeon <aha9654@gmail.com>
2024-03-22 17:50:08 +0900

    #22 수익/정산 관련 패키지 구조 (#28)
    * feat: 정산 관련 기본 패키지
    
    * feat: dto 추가

---
+++ src/main/java/com/pentoryall/revenue/controller/UserSettlementController.java
@@ -0,0 +1,11 @@
+package com.pentoryall.revenue.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+@RequiredArgsConstructor
+public class UserSettlementController {
+}
---
+++ src/main/java/com/pentoryall/revenue/mapper/UserSettlementMapper.java
@@ -0,0 +1,7 @@
+package com.pentoryall.revenue.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserSettlementMapper {
+}
