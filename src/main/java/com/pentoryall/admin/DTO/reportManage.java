fb8a8e2d507f6670a786913b15ab4232023aa3e8
jeong-gayeon <1bfish106@naver.com>
2024-03-22 15:51:43 +0900

    feat: 관리자 패키지설계

---
+++ src/main/java/com/pentoryall/admin/DTO/reportManage.java
@@ -0,0 +1,10 @@
+package com.pentoryall.admin.DTO;
+
+public class reportManage {
+
+    private long code;
+    private long userCode;
+    private long postCode;
+    private long commentCode;
+
+}
