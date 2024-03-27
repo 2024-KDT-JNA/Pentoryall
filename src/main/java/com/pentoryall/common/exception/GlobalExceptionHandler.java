2c8edbbde98af1f7ef59969ff920dd32dc590dd3
may54ther <aha9654@gmail.com>
2024-03-27 17:12:09 +0900

    feat: CustomException 상속 구조 변경

--- src/main/java/com/pentoryall/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pentoryall/common/exception/GlobalExceptionHandler.java
@@ -6,12 +6,19 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @Slf4j
-//@ControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public String errorView(Exception e, Model model) {
+    @ExceptionHandler(CustomException.class)
+    public String errorView(CustomException e, Model model) {
         model.addAttribute("errorMessage", e.getMessage());
+//        model.addAttribute("redirectUrl", "/");
         return "/error/errorPage";
     }
+
+//    @ExceptionHandler(Exception.class)
+//    public String errorView(Exception e, Model model) {
+//        model.addAttribute("errorMessage", e.getMessage());
+//        return "/error/errorPage";
+//    }
 }
