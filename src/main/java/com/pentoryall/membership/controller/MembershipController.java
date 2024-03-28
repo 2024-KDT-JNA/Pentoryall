9fe15da84c6e52e262d3e5f1dcf99be67f02810f
seungjaeshina <seungjaeshin96@gmail.com>
2024-03-28 11:54:02 +0900

    chore: application-default.yml 수정

--- src/main/java/com/pentoryall/membership/controller/MembershipController.java
+++ src/main/java/com/pentoryall/membership/controller/MembershipController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.Member;
 import java.time.LocalDateTime;
 
 
@@ -20,49 +23,12 @@ public class MembershipController {
         this.membershipService = membershipService;
     }
 
-
     @PostMapping("/create")
-    public ResponseEntity<String> createMembership(@RequestBody MembershipDTO membershipDTO){
+    public String createMembership(Model model, @ModelAttribute MembershipDTO membershipDTO) {
         MembershipDTO createMembership = membershipService.createMembership(membershipDTO);
-
-
-        return new ResponseEntity<>("멤버십이 성공적으로 개설되었습니다!" + createMembership.getName(), HttpStatus.CREATED);
+        model.addAttribute("message", "멤버십이 성공적으로 개설되었습니다! 이름: " + createMembership.getName());
+        return "/views/membership/successCreate";
     }
-
-    @PostMapping("/createForm")
-    public String createMembership(
-            @RequestParam("code") Long code,
-            @RequestParam("user_code") Long userCode,
-            @RequestParam("name") String name,
-            @RequestParam("introduction") String introduction,
-            @RequestParam("price") int price,
-            @RequestParam("color") String color,
-            @RequestParam("is_active") boolean isActive,
-            @RequestParam("is_deleted") boolean isDeleted,
-            @RequestParam("create_date") LocalDateTime createDate,
-            @RequestParam("update_date") LocalDateTime updateDate,
-            @RequestParam("delete_date") LocalDateTime deleteDate) {
-
-        System.out.println("코드: " + code);
-        System.out.println("유저 코드: " + userCode);
-        System.out.println("이름: " + name);
-        System.out.println("소개: " + introduction);
-        System.out.println("가격: " + price);
-        System.out.println("색상: " + color);
-        System.out.println("활성화 여부: " + isActive);
-        System.out.println("삭제 여부: " + isDeleted);
-        System.out.println("생성 날짜: " + createDate);
-        System.out.println("수정 날짜: " + updateDate);
-        System.out.println("삭제 날짜: " + deleteDate);
-
-
-      return "redirect:/membership/myMembership";
-    }
-
-
-
-
-
 
     /* 페이지 메핑 */
     @GetMapping("/list")
@@ -81,3 +47,43 @@ public class MembershipController {
     }
 
 }
+
+//    @PostMapping("/create")
+//    public ResponseEntity<String> createMembership(@RequestBody MembershipDTO membershipDTO){
+//        MembershipDTO createMembership = membershipService.createMembership(membershipDTO);
+//
+//
+//        return new ResponseEntity<>("멤버십이 성공적으로 개설되었습니다!" + createMembership.getName(), HttpStatus.CREATED);
+//    }
+//
+//    @PostMapping("/createForm")
+//    public String createMembership(
+//            @RequestParam("code") Long code,
+//            @RequestParam("user_code") Long userCode,
+//            @RequestParam("name") String name,
+//            @RequestParam("introduction") String introduction,
+//            @RequestParam("price") int price,
+//            @RequestParam("color") String color,
+//            @RequestParam("is_active") boolean isActive,
+//            @RequestParam("is_deleted") boolean isDeleted,
+//            @RequestParam("create_date") LocalDateTime createDate,
+//            @RequestParam("update_date") LocalDateTime updateDate,
+//            @RequestParam("delete_date") LocalDateTime deleteDate) {
+//
+//        System.out.println("코드: " + code);
+//        System.out.println("유저 코드: " + userCode);
+//        System.out.println("이름: " + name);
+//        System.out.println("소개: " + introduction);
+//        System.out.println("가격: " + price);
+//        System.out.println("색상: " + color);
+//        System.out.println("활성화 여부: " + isActive);
+//        System.out.println("삭제 여부: " + isDeleted);
+//        System.out.println("생성 날짜: " + createDate);
+//        System.out.println("수정 날짜: " + updateDate);
+//        System.out.println("삭제 날짜: " + deleteDate);
+//
+//
+//      return "redirect:/membership/myMembership";
+//    }
+
+
