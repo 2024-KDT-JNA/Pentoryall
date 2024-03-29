6f5a894f76c2ac5aa428e1d4c9d75c0f2117b6a3
eurdream98 <k01054792361@gmail.com>
2024-03-29 10:34:42 +0900

    feature: 비동기 통신으로 장르 가져오기

--- src/main/java/com/pentoryall/genre/controller/GenreController.java
+++ src/main/java/com/pentoryall/genre/controller/GenreController.java
@@ -2,16 +2,12 @@ package com.pentoryall.genre.controller;
 
 import com.pentoryall.genre.dto.GenreDTO;
 import com.pentoryall.genre.service.GenreService;
-import com.pentoryall.genreOfArt.dto.GenreOfArtDTO;
-import com.pentoryall.genreOfArt.service.GenreOfArtService;
-import com.pentoryall.series.service.SeriesService;
+import com.pentoryall.series.dto.SeriesDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
