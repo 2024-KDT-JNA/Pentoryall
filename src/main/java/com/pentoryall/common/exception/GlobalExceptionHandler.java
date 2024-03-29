050c9894c5c850185fa81167b0d2a2b892122209
may54ther <aha9654@gmail.com>
2024-03-29 19:17:27 +0900

    fix: model.attributeName 수정

--- src/main/java/com/pentoryall/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pentoryall/common/exception/GlobalExceptionHandler.java
@@ -12,7 +12,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(CustomException.class)
     public String errorView(CustomException e, Model model) {
         model.addAttribute("errorMessage", e.getMessage());
-        model.addAttribute("redirect", "/");
+        model.addAttribute("redirectUrl", "/");
         return "views/common/messageAlert";
     }
 
