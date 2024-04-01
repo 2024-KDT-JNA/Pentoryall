45f190b10478e35e93805938c8fb7a1263ca48fb
may54ther <aha9654@gmail.com>
2024-04-01 17:47:52 +0900

    feat: 회원 정산 정보 페이지

--- src/main/java/com/pentoryall/settlement/controller/UserSettlementController.java
+++ src/main/java/com/pentoryall/settlement/controller/UserSettlementController.java
@@ -2,10 +2,16 @@ package com.pentoryall.settlement.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping
 @RequiredArgsConstructor
 public class UserSettlementController {
+
+    @GetMapping("/user/settlement")
+    public String getUserSettlement() {
+        return "views/user/settlement/add";
+    }
 }
