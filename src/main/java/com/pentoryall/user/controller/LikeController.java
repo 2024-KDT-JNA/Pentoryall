5d2e02ade1ea4bd6a485e194f350ce96f090600c
yunii2222 <yunii2222@naver.com>
2024-04-01 15:36:29 +0900

    feat: 좋아요 기능 수정

--- src/main/java/com/pentoryall/user/controller/LikeController.java
+++ src/main/java/com/pentoryall/user/controller/LikeController.java
@@ -5,6 +5,7 @@ import com.pentoryall.user.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -19,7 +20,15 @@ public class LikeController {
     public String likePage() {
         return "views/user/likePage";
     }
-    
+
+    @GetMapping("/likeInfo")
+    public String getLikeInfo(Model model, @RequestParam("postCode") int postCode, @RequestParam("userCode") String userCode) {
+
+        model.addAttribute("board_no", postCode);
+        model.addAttribute("user_no", userCode);
+        return "likeInfoPage"; // 좋아요 정보를 표시할 페이지의 이름
+    }
+
     @ResponseBody
     @PostMapping("/likeUp")
     public void likeUp(@RequestBody LikeDTO likeDTO) {
