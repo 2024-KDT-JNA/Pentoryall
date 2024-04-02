816e25f087e1796a2202bdc2f78ae76ab1fb69fc
may54ther <aha9654@gmail.com>
2024-04-02 10:36:01 +0900

    feat: 정산 정보 조회 기능

--- src/main/java/com/pentoryall/settlement/controller/UserSettlementController.java
+++ src/main/java/com/pentoryall/settlement/controller/UserSettlementController.java
@@ -1,17 +1,49 @@
 package com.pentoryall.settlement.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.pentoryall.settlement.dto.UserSettlementDTO;
+import com.pentoryall.settlement.service.UserSettlementService;
+import com.pentoryall.user.dto.UserDTO;
+import groovy.util.logging.Slf4j;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Slf4j
 @Controller
-@RequestMapping
+@RequestMapping("/user/settlement")
 @RequiredArgsConstructor
 public class UserSettlementController {
 
-    @GetMapping("/user/settlement")
-    public String getUserSettlement() {
+    private final UserSettlementService userSettlementService;
+
+    @GetMapping
+    public String getUserSettlement(@AuthenticationPrincipal UserDTO sessionUser, Model model) throws AuthenticationException {
+        UserSettlementDTO selectedUserSettlement = userSettlementService.selectByUserCode(sessionUser.getCode());
+        if (selectedUserSettlement == null) {
+            model.addAttribute("userSettlement", new UserSettlementDTO());
+        } else {
+            model.addAttribute("userSettlement", selectedUserSettlement);
+        }
+
         return "views/user/settlement/add";
+    }
+
+    @PostMapping
+    public String addUserSettlement(@ModelAttribute("userSettlement") UserSettlementDTO modifyUserSettlement,
+                                    @AuthenticationPrincipal UserDTO sessionUser,
+                                    RedirectAttributes rttr) {
+        // userSettlementService.insertOrDeleteUserSettlement();
+
+        System.out.println(modifyUserSettlement);
+        rttr.addAttribute("userSettlement", userSettlementService.selectByUserCode(sessionUser.getCode()));
+        return "redirect:";
     }
 }
