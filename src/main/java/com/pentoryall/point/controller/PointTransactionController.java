43801c5ca0a3b3f56700341188b03da68f9041e8
may54ther <aha9654@gmail.com>
2024-04-04 17:53:27 +0900

    chore: 컨트롤러명 변경과 서비스, 매퍼 저장

--- src/main/java/com/pentoryall/point/controller/PointTransactionController.java
+++ src/main/java/com/pentoryall/point/controller/PointTransactionController.java
@@ -1,11 +1,43 @@
 package com.pentoryall.point.controller;
 
+import com.pentoryall.point.service.PointTransactionService;
+import com.pentoryall.user.dto.UserDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping()
 @RequiredArgsConstructor
 public class PointTransactionController {
+
+    // TODO
+    // 나중에. 구매된 컨텐츠의 경우는 알려줘야됨..조인?
+    // 멤버십도 스토리 같은 곳 가면 이미 구매내역있으면 알려줘야되네..
+    // private final     PostService postService;
+    // private final     PostService postService;
+    private final PointTransactionService pointTransactionService;
+
+    public ResponseEntity<String> purchase(Long postCode,
+                                           @AuthenticationPrincipal UserDTO sessionUser) {
+        // isAlreadyPurchaseByPostCode
+
+        return null;
+    }
+    //     작품 결제
+    // 이미 구매 내역이 있으면 : AlreadyHavePurchaseHistoryException?
+    // 아니면. 구매 진행~
+
+    //     멤버십 구매
+    //     request: 회원정보, 멤버십 코드, 가격?
+    // 검증: 이미 가입 시 .. 이미 가입되있음을 알려주는 response
+    // 서비스:
+
+    //
+
+    // 포인트 거래 내역
+
+    // 작품 구매 내역
 }
--- src/main/java/com/pentoryall/point/service/TransactionService.java
+++ src/main/java/com/pentoryall/point/service/TransactionService.java
@@ -1,7 +1,37 @@
 package com.pentoryall.point.service;
 
+import com.pentoryall.point.dto.TransactionDTO;
+import com.pentoryall.point.enums.TransactionType;
+import com.pentoryall.point.mapper.TransactionMapper;
+import com.pentoryall.user.dto.UserDTO;
+import com.pentoryall.user.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class TransactionService {
+
+    private final TransactionMapper pointTransactionMapper;
+    private final UserMapper userMapper;
+
+
+    public void purchase(TransactionDTO transaction, UserDTO sessionUser, UserDTO sellerUser) {
+        // 작품 구매
+        if (transaction.getType() == TransactionType.POST) {
+            pointTransactionMapper.save(transaction);
+        }
+        // UserDTO buyer = new UserDTO();
+
+        // UserDTO seller = new UserDTO();
+    }
 }
+
+
+// 거래 테이블에 내역 넣기?
+// 구매자 포인트 차감..
+// 판매자 포인트 증가..
+
+// if(isAlreadyPurchaseByMembershipCode()){
+//
+// }
