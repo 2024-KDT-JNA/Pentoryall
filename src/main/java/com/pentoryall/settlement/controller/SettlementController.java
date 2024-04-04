518382de3d038a99f443ec5b985ecbcd50d43577
may54ther <aha9654@gmail.com>
2024-04-05 01:48:43 +0900

    feat: Controller 경로 지정

--- src/main/java/com/pentoryall/settlement/controller/SettlementController.java
+++ src/main/java/com/pentoryall/settlement/controller/SettlementController.java
@@ -1,11 +1,30 @@
 package com.pentoryall.settlement.controller;
 
+import com.pentoryall.settlement.service.SettlementService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("/settings")
 @RequiredArgsConstructor
 public class SettlementController {
+
+    SettlementService settlementService;
+
+    @GetMapping("/settlement")
+    public String settlement() {
+        return "views/settlement/settlement";
+    }
+
+    @GetMapping("/settlement/list")
+    public String settlementListPage() {
+        return "views/settlement/settlementList";
+    }
+
+    @GetMapping("/revenue/list")
+    public String revenueListPage() {
+        return "views/settlement/revenueList";
+    }
 }
