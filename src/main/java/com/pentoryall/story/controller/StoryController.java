9cb7458f574839fada57fb316180987abf3c3b13
eurdream98 <k01054792361@gmail.com>
2024-04-08 21:53:41 +0900

    feature: 마이스토리 페이지에서 포스트,시리즈 탭에서 로그인 한 회원의 포스트,시리즈 확인 탭

--- src/main/java/com/pentoryall/story/controller/StoryController.java
+++ src/main/java/com/pentoryall/story/controller/StoryController.java
@@ -1,6 +1,10 @@
 package com.pentoryall.story.controller;
 
 import com.pentoryall.common.exception.PageNotFoundException;
+import com.pentoryall.post.dto.PostDTO;
+import com.pentoryall.post.service.PostService;
+import com.pentoryall.series.dto.SeriesDTO;
+import com.pentoryall.series.service.SeriesService;
 import com.pentoryall.story.dto.StoryUserDTO;
 import com.pentoryall.user.dto.UserDTO;
 import com.pentoryall.user.service.UserService;
@@ -11,11 +15,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Collections;
+import java.util.List;
+
+import static com.fasterxml.jackson.databind.type.LogicalType.Collection;
+
 @Controller
 @RequiredArgsConstructor
 public class StoryController {
 
     private final UserService userService;
+    private final PostService postService;
+    private final SeriesService seriesService;
 
     @GetMapping("/story")
     public String storyPage(@AuthenticationPrincipal UserDTO sessionUser) {
@@ -35,6 +46,35 @@ public class StoryController {
         model.addAttribute("storyUser", new StoryUserDTO(selectedUser));
 
         return "/views/story/home";
+    }
+
+    @GetMapping("/{userId}/posts")
+    public String storyPostsPage(@PathVariable("userId")String userId,
+                                 Model model){
+        UserDTO selectedUser = userService.selectByUserId(userId);
+        if (selectedUser == null) {
+            throw new PageNotFoundException();
+        }
+        List<PostDTO> postList = postService.selectPostByUserCode(selectedUser.getCode());
+        Collections.reverse(postList);
+        model.addAttribute("storyUser", new StoryUserDTO(selectedUser));
+        model.addAttribute("postList",postList);
+        return "/views/story/posts";
+    }
+
+    @GetMapping("/{userId}/series")
+    public String storySeriesPage(@PathVariable("userId")String userId,
+                                 Model model){
+        UserDTO selectedUser = userService.selectByUserId(userId);
+        if (selectedUser == null) {
+            throw new PageNotFoundException();
+        }
+        List<SeriesDTO> seriesList = seriesService.getSeriesList(selectedUser.getCode());
+        Collections.reverse(seriesList);
+        System.out.println("seriesList = " + seriesList);
+        model.addAttribute("storyUser", new StoryUserDTO(selectedUser));
+        model.addAttribute("seriesList",seriesList);
+        return "/views/story/series";
     }
 }
 
