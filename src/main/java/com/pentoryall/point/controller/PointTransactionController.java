27642fcbef1000bbf4b481a141d1a5452ead63ed
may54ther <aha9654@gmail.com>
2024-04-09 20:13:03 +0900

    feat: 포인트 거래 컨트롤러 분리

--- src/main/java/com/pentoryall/point/controller/PointTransactionController.java
+++ src/main/java/com/pentoryall/point/controller/PointTransactionController.java
@@ -1,27 +1,23 @@
 package com.pentoryall.point.controller;
 
-import com.pentoryall.common.dto.CommonResponse;
 import com.pentoryall.membership.dto.MembershipDTO;
 import com.pentoryall.membership.service.MembershipService;
-import com.pentoryall.point.service.TransactionService;
-import com.pentoryall.post.dto.PostDTO;
-import com.pentoryall.post.service.PostService;
 import com.pentoryall.user.dto.UserDTO;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-@RequiredArgsConstructor
 public class PointTransactionController {
 
-    private final TransactionService transactionService;
-    private final PostService postService;
     private final MembershipService membershipService;
+
+    @Autowired
+    public PointTransactionController(MembershipService membershipService) {
+        this.membershipService = membershipService;
+    }
 
     @GetMapping("/membership/register")
     public String getRegisterMembershipPage(Long membershipCode, @AuthenticationPrincipal UserDTO sessionUser, Model model) {
@@ -32,22 +28,4 @@ public class PointTransactionController {
         }
         return "views/membership/register";
     }
-
-    @PostMapping("/transaction/post")
-    public String postTransaction(Long postCode, @AuthenticationPrincipal UserDTO sessionUser) {
-        PostDTO post = postService.getPostInformationByPostCode(postCode);
-        transactionService.postTransaction(post, sessionUser);
-
-        return "redirect:/post/information?code=" + postCode;
-    }
-
-    @PostMapping("/transaction/membership")
-    public ResponseEntity<CommonResponse> membershipTransaction(Long membershipCode, @AuthenticationPrincipal UserDTO sessionUser) {
-
-        MembershipDTO membership = membershipService.selectMembershipByCode(membershipCode);
-        transactionService.membershipTransaction(membership, sessionUser);
-
-        return null;
-    }
-
 }
