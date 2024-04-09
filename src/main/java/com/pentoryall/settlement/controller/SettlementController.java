ca2e58911da06003a67accb1c0e49f31ef02f3e8
may54ther <aha9654@gmail.com>
2024-04-09 20:21:25 +0900

    feat: 수익/정산 관리 페이지와 경로 연결

--- src/main/java/com/pentoryall/settlement/controller/SettlementController.java
+++ src/main/java/com/pentoryall/settlement/controller/SettlementController.java
@@ -1,20 +1,57 @@
 package com.pentoryall.settlement.controller;
 
+import com.pentoryall.settlement.dto.SettlementDTO;
+import com.pentoryall.settlement.dto.UserSettlementDTO;
+import com.pentoryall.settlement.enums.SettlementState;
 import com.pentoryall.settlement.service.SettlementService;
-import lombok.RequiredArgsConstructor;
+import com.pentoryall.settlement.service.UserSettlementService;
+import com.pentoryall.user.dto.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/settings")
-@RequiredArgsConstructor
 public class SettlementController {
 
-    SettlementService settlementService;
+    private final SettlementService settlementService;
+    private final UserSettlementService userSettlementService;
+
+    @Autowired
+    public SettlementController(SettlementService settlementService, UserSettlementService userSettlementService) {
+        this.settlementService = settlementService;
+        this.userSettlementService = userSettlementService;
+    }
 
     @GetMapping("/settlement")
-    public String settlement() {
+    public String settlementPage(@AuthenticationPrincipal UserDTO sessionUser, Model model) {
+
+        UserSettlementDTO userSettlement = userSettlementService.selectByUserCode(sessionUser.getCode());
+        if (userSettlement != null) {
+            SettlementDTO settlement = new SettlementDTO();
+            settlement.setUserSettlementCode(userSettlement.getCode());
+            settlement.setRequestAmount(sessionUser.getRevenue());
+            settlement.setActualAmount((sessionUser.getRevenue() / 10) * 9);
+            settlement.setState(SettlementState.REQUESTED);
+
+            model.addAttribute("settlement", settlement);
+        }
+
+        return "views/settlement/settlement";
+    }
+
+    @PostMapping("/settlement")
+    public String settlement(@AuthenticationPrincipal UserDTO sessionUser, Model model) {
+
+        UserSettlementDTO userSettlement = userSettlementService.selectByUserCode(sessionUser.getCode());
+        if (userSettlement != null) {
+            model.addAttribute("userSettlement", userSettlement);
+        }
+
         return "views/settlement/settlement";
     }
 
@@ -28,3 +65,4 @@ public class SettlementController {
         return "views/settlement/revenueList";
     }
 }
+
