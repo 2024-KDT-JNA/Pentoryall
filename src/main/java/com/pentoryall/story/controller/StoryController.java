d8b5c373d820d705afb603016b68e1d901d293be
may54ther <aha9654@gmail.com>
2024-04-09 17:35:20 +0900

    fix: story 컨트롤러 경로 수정

--- src/main/java/com/pentoryall/story/controller/StoryController.java
+++ src/main/java/com/pentoryall/story/controller/StoryController.java
@@ -14,74 +14,71 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Collections;
 import java.util.List;
 
-import static com.fasterxml.jackson.databind.type.LogicalType.Collection;
-
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/story")
 public class StoryController {
 
     private final UserService userService;
     private final PostService postService;
     private final SeriesService seriesService;
 
-    @GetMapping("/story")
+    @GetMapping
     public String storyPage(@AuthenticationPrincipal UserDTO sessionUser) {
         if (sessionUser == null)
             throw new PageNotFoundException();
-        return "redirect:/" + sessionUser.getUserId();
+        return "redirect:/story/" + sessionUser.getUserId();
     }
 
     @GetMapping("/{userId}")
     public String storyPage(@PathVariable("userId") String userId, Model model) {
-        
-        UserDTO selectedUser = userService.selectByUserId(userId);
-        if (selectedUser == null) {
-            throw new PageNotFoundException();
-        }
+
+        UserDTO selectedUser = getUserOrNotFoundException(userId);
         List<PostDTO> postList = postService.selectPostByUserCode(selectedUser.getCode());
         model.addAttribute("storyUser", new StoryUserDTO(selectedUser));
 
         Collections.reverse(postList);
-        model.addAttribute("post",postList.get(0));
+        model.addAttribute("post", postList.get(0));
 
         List<SeriesDTO> seriesList = seriesService.getSeriesList(selectedUser.getCode());
         Collections.reverse(seriesList);
-        model.addAttribute("series",seriesList.get(0));
+        model.addAttribute("series", seriesList.get(0));
 
         return "/views/story/home";
     }
 
     @GetMapping("/{userId}/posts")
-    public String storyPostsPage(@PathVariable("userId")String userId,
-                                 Model model){
-        UserDTO selectedUser = userService.selectByUserId(userId);
-        if (selectedUser == null) {
-            throw new PageNotFoundException();
-        }
+    public String storyPostsPage(@PathVariable("userId") String userId, Model model) {
+        UserDTO selectedUser = getUserOrNotFoundException(userId);
         List<PostDTO> postList = postService.selectPostByUserCode(selectedUser.getCode());
         Collections.reverse(postList);
         model.addAttribute("storyUser", new StoryUserDTO(selectedUser));
-        model.addAttribute("postList",postList);
+        model.addAttribute("postList", postList);
         return "/views/story/posts";
     }
 
     @GetMapping("/{userId}/series")
-    public String storySeriesPage(@PathVariable("userId")String userId,
-                                 Model model){
-        UserDTO selectedUser = userService.selectByUserId(userId);
-        if (selectedUser == null) {
-            throw new PageNotFoundException();
-        }
+    public String storySeriesPage(@PathVariable("userId") String userId, Model model) {
+        UserDTO selectedUser = getUserOrNotFoundException(userId);
         List<SeriesDTO> seriesList = seriesService.getSeriesList(selectedUser.getCode());
         Collections.reverse(seriesList);
         System.out.println("seriesList = " + seriesList);
         model.addAttribute("storyUser", new StoryUserDTO(selectedUser));
-        model.addAttribute("seriesList",seriesList);
+        model.addAttribute("seriesList", seriesList);
         return "/views/story/series";
+    }
+
+    private UserDTO getUserOrNotFoundException(String userId) {
+        UserDTO selectedUser = userService.selectByUserId(userId);
+        if (selectedUser == null) {
+            throw new PageNotFoundException();
+        } else
+            return selectedUser;
     }
 }
 
