5baba8897364d91f5ac7696c7bbc0d43873bbef3
may54ther <aha9654@gmail.com>
2024-04-11 01:22:19 +0900

    feat: 정산 요청 기능 구현

--- src/main/java/com/pentoryall/settlement/service/SettlementService.java
+++ src/main/java/com/pentoryall/settlement/service/SettlementService.java
@@ -5,8 +5,11 @@ import com.pentoryall.common.page.SelectCriteria;
 import com.pentoryall.settlement.dto.SettlementDTO;
 import com.pentoryall.settlement.dto.UserRevenueDTO;
 import com.pentoryall.settlement.mapper.SettlementMapper;
+import com.pentoryall.user.dto.UserDTO;
+import com.pentoryall.user.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
 import java.util.List;
@@ -17,6 +20,20 @@ import java.util.Map;
 public class SettlementService {
 
     private final SettlementMapper settlementMapper;
+
+    private final UserMapper userMapper;
+
+    @Transactional
+    public void insertSettlementRequest(Long userCode, SettlementDTO settlement) {
+
+        settlementMapper.insertSettlementRequest(settlement);
+
+        if (settlement.getCode() > 0) {
+            UserDTO selectedUser = userMapper.getUserInformationByUserCode(userCode);
+            selectedUser.setRevenue(selectedUser.getRevenue() - settlement.getRequestAmount());
+            userMapper.updateRevenueByUserCode(selectedUser);
+        }
+    }
 
     public Map<String, Object> selectSettlementListWithPagingByUserCode(int page, long userCode) {
 
