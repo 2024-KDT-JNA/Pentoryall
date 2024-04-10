69929a820319e80d7b03ac86705377fb35134048
may54ther <aha9654@gmail.com>
2024-04-10 20:34:19 +0900

    feat: 서비스, 컨트롤러 변경

--- src/main/java/com/pentoryall/settlement/service/SettlementService.java
+++ src/main/java/com/pentoryall/settlement/service/SettlementService.java
@@ -1,7 +1,46 @@
 package com.pentoryall.settlement.service;
 
+import com.pentoryall.common.page.Pagination;
+import com.pentoryall.common.page.SelectCriteria;
+import com.pentoryall.settlement.dto.SettlementDTO;
+import com.pentoryall.settlement.dto.UserRevenueDTO;
+import com.pentoryall.settlement.mapper.SettlementMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Service
+@RequiredArgsConstructor
 public class SettlementService {
+
+    private final SettlementMapper settlementMapper;
+
+    public Map<String, Object> selectSettlementListWithPagingByUserCode(int page, long userCode) {
+
+        int totalCount = settlementMapper.selectTotalSettlementCountByUserCode(userCode);
+        SelectCriteria selectCriteria = Pagination.getSelectCriteria(page, totalCount);
+
+        Map<String, Object> settlementListAndPaging = new HashMap<>();
+        List<SettlementDTO> settlementList = settlementMapper.selectSettlementsByUserCode(userCode, selectCriteria.getOffset(), selectCriteria.getLimit());
+        settlementListAndPaging.put("paging", selectCriteria);
+        settlementListAndPaging.put("settlementList", settlementList);
+
+        return settlementListAndPaging;
+    }
+
+    public Map<String, Object> selectRevenueListWithPagingByUserCode(int page, long userCode) {
+
+        int totalCount = settlementMapper.selectTotalRevenueCountByUserCode(userCode);
+        SelectCriteria selectCriteria = Pagination.getSelectCriteria(page, totalCount);
+
+        Map<String, Object> revenueListAndPaging = new HashMap<>();
+        List<UserRevenueDTO> revenueList = settlementMapper.selectRevenuesByUserCode(userCode, selectCriteria.getOffset(), selectCriteria.getLimit());
+        revenueListAndPaging.put("paging", selectCriteria);
+        revenueListAndPaging.put("revenueList", revenueList);
+
+        return revenueListAndPaging;
+    }
 }
