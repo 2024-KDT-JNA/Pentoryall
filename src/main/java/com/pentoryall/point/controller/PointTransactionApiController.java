e7a28617eed94845e484077e8cc8389be8381710
아현 Ahyeon <aha9654@gmail.com>
2024-04-10 11:34:49 +0900

    스토리 홈 오류 수정 및 탭 처리 (#164)
    * feat: 포스트 구매 시 세션 정보 갱신
    
    * fix: story 메인 페이지의 에러 수정
    
    * feat: 스토리 페이지 탭 처리

--- src/main/java/com/pentoryall/point/controller/PointTransactionApiController.java
+++ src/main/java/com/pentoryall/point/controller/PointTransactionApiController.java
@@ -7,9 +7,14 @@ import com.pentoryall.point.service.TransactionService;
 import com.pentoryall.post.dto.PostDTO;
 import com.pentoryall.post.service.PostService;
 import com.pentoryall.user.dto.UserDTO;
+import com.pentoryall.user.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +24,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/transaction")
 public class PointTransactionApiController {
 
+    private final AuthService authService;
     private final TransactionService transactionService;
     private final PostService postService;
     private final MembershipService membershipService;
 
     @Autowired
-    public PointTransactionApiController(TransactionService transactionService, PostService postService, MembershipService membershipService) {
+    public PointTransactionApiController(AuthService authService, TransactionService transactionService, PostService postService, MembershipService membershipService) {
+        this.authService = authService;
         this.transactionService = transactionService;
         this.postService = postService;
         this.membershipService = membershipService;
@@ -37,7 +44,11 @@ public class PointTransactionApiController {
         if (sessionUser.getPoint() < post.getPrice()) {
             return ResponseEntity.ok(new CommonResponse(false, "포인트가 부족하여 구매를 실패하였습니다.", null));
         }
+
         transactionService.postTransaction(post, sessionUser);
+
+        // 세션 정보 재설정
+        SecurityContextHolder.getContext().setAuthentication(createNewAuthentication(sessionUser.getUserId()));
 
         return ResponseEntity.ok(new CommonResponse(true, "구매가 완료되었습니다.", null));
     }
@@ -48,5 +59,12 @@ public class PointTransactionApiController {
         transactionService.membershipTransaction(membership, sessionUser);
 
         return ResponseEntity.ok(new CommonResponse(true, "가입이 완료되었습니다.", membership.getCode()));
+    }
+
+
+    // TODO 중복코드 차후에 합치기
+    protected Authentication createNewAuthentication(String userId) {
+        UserDetails newPrincipal = authService.loadUserByUsername(userId);
+        return new UsernamePasswordAuthenticationToken(newPrincipal, newPrincipal.getPassword(), newPrincipal.getAuthorities());
     }
 }
