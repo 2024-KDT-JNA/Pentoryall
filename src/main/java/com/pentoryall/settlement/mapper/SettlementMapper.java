005f9bf2bdf18f490bc2e443c3fbe5bd5d5fa741
아현 Ahyeon <aha9654@gmail.com>
2024-04-10 20:42:53 +0900

    #83, 172 수익 적립 내역, 정산 요청 내역 (#173)
    * refactor: 생성자 수정
    
    * feat: 회원의 수익 정보 DTO
    
    * feat: 수익 적립 내역, 정산 요청 내역 sql
    
    * feat: 서비스, 컨트롤러 변경
    
    * feat: 페이지 적용

--- src/main/java/com/pentoryall/settlement/mapper/SettlementMapper.java
+++ src/main/java/com/pentoryall/settlement/mapper/SettlementMapper.java
@@ -1,7 +1,19 @@
 package com.pentoryall.settlement.mapper;
 
+import com.pentoryall.settlement.dto.SettlementDTO;
+import com.pentoryall.settlement.dto.UserRevenueDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface SettlementMapper {
+
+    int selectTotalSettlementCountByUserCode(long userCode);
+
+    int selectTotalRevenueCountByUserCode(long userCode);
+
+    List<SettlementDTO> selectSettlementsByUserCode(long userCode, int offset, int limit);
+
+    List<UserRevenueDTO> selectRevenuesByUserCode(long userCode, int offset, int limit);
 }
